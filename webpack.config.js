5f8806dea501c5d93f4bc5d6060ca53f74d212aa
Nick Field <heyimnick@live.com>
2017-02-21 18:37:08 +0000

    add babel dev-dependencies to package.json

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  // Set entry file
+  entry: {
+    main: './src/lib/index.js'
+  },
+  // set output JS file
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, '/public/js')
+  },
+
+  module: {
+    loaders: [
+      // Processes Javascript from webpack-build.js
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader' // & transpile
+      }
+    ]
+  }
+};
