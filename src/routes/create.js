f0d4643a828579bec9ea5e2a75b254dae81dd782
Nick Field <heyimnick@live.com>
2017-02-21 17:17:46 +0000

    remove home handler content from create boilerplate

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,26 +1,11 @@
 module.exports = {
   method: ['GET', 'POST'],
-  path: '/',
-  config: {
-    auth: {
-      strategy: 'session',
-      mode: 'try'
-    },
-    plugins: {
-      'hapi-auth-cookie': {
-        redirectTo: false
-      },
-      handler: (req, reply) => {
-        if (req.method === 'get') {
-          if (req.auth.isAuthenticated) {
-            // Serve av-chat view
-          } else {
-            // Serve create view
-          }
-        } else {
+  path: '/create',
+  handler: (req, reply) => {
+    if (req.method === 'get') {
+          // Serve create post
+    } else {
           // Process create POST (store new room & endpoint) set Cookie and redirect to '/'
-        }
-      }
     }
   }
 };
