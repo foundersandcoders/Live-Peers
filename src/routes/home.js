28ae9bbe319f1a54eed5fe0e35f78b2e6c61492f
Nick Field <heyimnick@live.com>
2017-02-21 17:30:04 +0000

    update home route to factor in query for student

--- src/routes/home.js
+++ src/routes/home.js
@@ -8,6 +8,12 @@ module.exports = {
       get roomname, roomid and endpointid and inject it into view
       */
       reply.view('home', config);
+    } else if (req.query) {
+      // If student is trying to join room
+      let roomid = req.query.roomid;
+      let config = {};
+      // Look up room details and store in config
+      reply.view('chat-only', config);
     } else {
       reply.view('create');
     }
