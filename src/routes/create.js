4d9ac9fa751e37b1734506c23d0475f2496d52dc
Nick Field <heyimnick@live.com>
2017-02-21 17:50:19 +0000

    add if branches in create route

--- src/routes/create.js
+++ src/routes/create.js
@@ -2,11 +2,14 @@ module.exports = {
   method: 'POST',
   path: '/create',
   handler: (req, reply) => {
-    // Logic to handle if authenticated
-    // if authentiated, process create POST (store new room & endpoint), then
-    reply.redirect('/');
-    // else
-    let loginfailmessage = {};
-    reply.view('create', loginfailmessage);
+    // Mentor signing in & creating room
+    if (req.auth.isAuthenticated) {
+      // if authentiated, process create POST (store new room & endpoint), then redirects to home
+      reply.redirect('/');
+    } else {
+      let loginfailmessage = {};
+      // Make fail message
+      reply.view('create', loginfailmessage);
+    }
   }
 };
