a591c195176d16f79cebf6140d325f955c33d66c
Nick Field <heyimnick@live.com>
2017-02-21 18:22:04 +0000

    add client comms boilerplate tests

---
+++ tests/clientcomms.js
@@ -0,0 +1,7 @@
+// Use Tape to test (webpack is needed for client side testing [using 'require' pattern]
+
+// Adding endpoint and room id to new comms instance
+// Adding handlers
+// Calling handlers
+// Mocking socket sending/receiving
+// Adding external endpoints
