178a9caf61880744e9a5bb26be0dbb548e1d9014
Nick Field <heyimnick@live.com>
2017-02-27 16:55:29 +0000

    update index.js to start socket server

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const server = require('./src/server.js');
+// Import regular server file
+const server = require('./src/server');
+// Import socket server file
+const socketserver = require('./src/socketrouter');
+// Require socket.io
+const io = require('socket.io');
 
-server.start((err) => {
-  if (err) { throw err; }
-  console.log(`Server is running at ${server.info.uri}`);
+const port = 8080;
+
+// Start REST server
+server.start(() => {
+  console.log(`Server live at https://localhost:${port}/`);
 });
+
+// Start Websocket server from server listener (hapi implementation)
+socketserver(io(server.listener));
