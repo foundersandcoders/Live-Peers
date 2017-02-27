595e45c85e0bd69be5656599680208c040d04e1b
jsms90 <jsms@hotmail.co.uk>
2017-02-27 03:16:03 +0000

    add create route
    relates #54

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,7 +1,29 @@
+const Rooms = require('../../rooms.js');
+
 module.exports = {
+  method: 'POST',
   path: '/create',
-  method: 'get',
-  handler : (req, res) => {
-    // create route
+  handler: (req, reply) => {
+    // Verify username and password - in a later sprint
+    // if (user is authenticated) {
+      let roomId = Math.random().toString(36).slice(2);
+      let endpointId = Math.random().toString(36).slice(2);
+
+      let Rooms[roomId] = roomId;
+      let Rooms[roomName] = req.body.roomName;
+      let Rooms[req.body.pin] = Math.floor(Math.random() * 10000);
+      let Rooms.endpoints[endpointId] = {
+        username: req.body.username,
+        permissions: ['CHAT', 'AV']
+      };
+
+      req.cookieAuth.set({ roomId: roomId, endpointId: endpointId });
+
+      reply.redirect('/');
+    // } else {
+    //   let loginfailmessage = checkLoginDetails()
+    //     where checkLoginDetails returns { wrongUsernameMessage, wrongPasswordMessage}
+    //   reply.view('create', loginfailmessage);
+    // }
   }
-}
+};
