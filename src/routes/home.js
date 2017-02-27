15ef654ca941be59e2e44350a1f0ec6148c2b342
Nick Field <heyimnick@live.com>
2017-02-27 18:39:43 +0000

    add template data to be injected into home view

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,39 +1,57 @@
-const { getRoomData, getUserData } = require('./handlers/roomQueries');
+// const { getRoomData, getUserData } = require('./handlers/roomQueries');
+//
+// module.exports = {
+//   method: 'GET',
+//   path: '/',
+//   handler: (req, reply) => {
+//     // Student branch
+//     if (req.query.roomId) {
+//       if (req.auth.credentials) {
+//         if (req.auth.isAuthenticated) {
+//           let roomId = req.auth.credentials.roomId;
+//           let endpointId = req.auth.credentials.endpointId;
+//           let roomData = getUserData(roomId, endpointId);
+//           reply.view('main', roomData);
+//         }
+//         else {
+//           reply.view('join', { errorMessage: "Incorrect credentials" });
+//         }
+//       }
+//       else {
+//         let roomId = req.query.roomId;
+//         let roomData = getRoomData(roomId);
+//         reply.view('join', roomData);
+//       }
+//     }
+//     // Mentor branch
+//     else {
+//       if (req.auth.isAuthenticated) {
+//         let roomId = req.auth.credentials.roomId;
+//         let endpointId = req.auth.credentials.endpointId;
+//         let userData = getUserData(roomId, endpointId);
+//         reply.view('main', userData);
+//       }
+//       else {
+//         reply.view('create');
+//       }
+//     }
+//   }
+// };
 
 module.exports = {
   method: 'GET',
   path: '/',
   handler: (req, reply) => {
-    // Student branch
-    if (req.query.roomId) {
-      if (req.auth.credentials) {
-        if (req.auth.isAuthenticated) {
-          let roomId = req.auth.credentials.roomId;
-          let endpointId = req.auth.credentials.endpointId;
-          let roomData = getUserData(roomId, endpointId);
-          reply.view('main', roomData);
-        }
-        else {
-          reply.view('join', { errorMessage: "Incorrect credentials" });
-        }
-      }
-      else {
-        let roomId = req.query.roomId;
-        let roomData = getRoomData(roomId);
-        reply.view('join', roomData);
-      }
-    }
-    // Mentor branch
-    else {
-      if (req.auth.isAuthenticated) {
-        let roomId = req.auth.credentials.roomId;
-        let endpointId = req.auth.credentials.endpointId;
-        let userData = getUserData(roomId, endpointId);
-        reply.view('main', userData);
-      }
-      else {
-        reply.view('create');
-      }
-    }
+
+    // For Demo
+    const viewData = {
+      roomId: 'fGhsAze',
+      roomName: 'mycoolroom',
+      pin: '1435',
+      endpointId: 'FhgOs',
+      username: 'John',
+    };
+
+    reply.view('home', viewData);
   }
 };
