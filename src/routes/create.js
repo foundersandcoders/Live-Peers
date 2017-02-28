b96852d8abe848fd95720d3caaff8f016ba8b869
Nick Field <heyimnick@live.com>
2017-02-28 11:49:10 +0000

    move pin creation from room class to /create route, redirect to home & refactor
    relates #54

--- src/routes/create.js
+++ src/routes/create.js
@@ -5,34 +5,23 @@ module.exports = {
   method: 'POST',
   path: '/create',
   handler: (req, reply) => {
-    // Verify username and password - in a later sprint
-    // if (user is authenticated) {
     let username = req.body.username;
     let roomName = req.body.roomName;
     let roomId = Math.random().toString(36).slice(2);
-    roomId = new Room(username, roomName, permissions);
-
-    let endpointId = Room.roomId.createEndpointId();
+    let pin = Math.floor(Math.random() * 8000) + 1000;
+    let endpointId = Math.random().toString(36).slice(2);
     let permissions = ['CHAT', 'AV'];
-    Room.roomId.addEndpoint(endpointId);
-    Room.roomId.updateUsername(endpointId, username);
-    Room.roomId.updatePermissions(endpointId, permissions);
+    Rooms[roomId] = new Room(roomName);
+    Rooms[roomId].addPin(pin);
+    Rooms[roomId].addEndpoint(endpointId);
+    Rooms[roomId].updateUsername(endpointId, username);
+    Rooms[roomId].updatePermissions(endpointId, permissions);
 
-    let data = {
-      roomId: roomId,
-      roomName: roomName,
-      endpointId: endpointId,
-      username: username,
-      permissions: permissions
-    };
+    req.cookieAuth.set({
+      roomId,
+      endpointId
+    });
 
-    req.cookieAuth.set({ roomId: roomId, endpointId: endpointId });
-
-    reply.view('main', data);
-    // } else {
-    //   let loginfailmessage = checkLoginDetails()
-    //     where checkLoginDetails returns { wrongUsernameMessage, wrongPasswordMessage}
-    //   reply.view('create', loginfailmessage);
-    // }
+    reply.redirect('/');
   }
 };
